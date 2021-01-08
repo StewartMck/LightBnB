03cb20b0d1b23f65e6486ba5faa39849ba3e35c9
Stewart <stewart.mckinlay@gmail.com>
2021-01-07 21:47:37 -0700

    refactor project

--- 1_queries/most_visited_cities.sql
+++
@@ -1,5 +0,0 @@
-SELECT city, COUNT(reservations.*) AS total_reservations
-FROM properties
-JOIN reservations ON properties.id = reservations.property_id
-GROUP by city
-ORDER BY total_reservations DESC;
